3ef987e43d66ffddca3bdb0caaf11e53c9d14169
pouriya <pouriya.jahanbakhsh@gmail.com>
2017-12-26 08:59:17 +0330

    Version 17.12.24

--- src/cfg.erl
+++ src/cfg.erl
@@ -31,7 +31,7 @@
 %%% POSSIBILITY OF SUCH DAMAGE.
 %%% ------------------------------------------------------------------------------------------------
 %% @author   Pouriya Jahanbakhsh <pouriya.jahanbakhsh@gmail.com>
-%% @version  17.11.14
+%% @version  17.12.26
 %% @doc
 %%           Simple and modern config parser.
 %% @end
